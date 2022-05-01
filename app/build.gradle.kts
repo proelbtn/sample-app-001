6a757af9bc12f680268066cfe5e4ab110e0c6fb4
Ryoga Saito <contact@proelbtn.com>
2022-05-01 20:24:47 +0900

    Introduce Timber

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,8 @@ dependencies {
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+
+    implementation("com.jakewharton.timber:timber:5.0.1")
 }
 
 kapt {
--- app/src/main/java/com/proelbtn/sampleapp001/MainApplication.kt
+++ app/src/main/java/com/proelbtn/sampleapp001/MainApplication.kt
@@ -2,6 +2,12 @@ package com.proelbtn.sampleapp001
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class MainApplication : Application()
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
